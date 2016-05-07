df868a1f67652aee505d74b1dd2d43c02dff8077
Maike <Maike.Fillmer@gmail.com>
2016-05-07 10:50:30 -0400

    Corrected storage of the answers

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -4,8 +4,15 @@
 
 // Dependencies
 // =============================================================
-var orm 			= require ("../config/orm.js");
+//var orm 			= require ("../config/orm.js");
+var friends 		= [];
+var MF	 			= {
+						name: "maike",
+						photo: "URL",
+						scores: [5,4,3,2,1,2,3,5,4,1]
+					}
 
+					friends.push(MF);
 
 // Routes
 // =============================================================
@@ -14,23 +21,27 @@ module.exports = function(app){
 	// Display all characters then provides JSON
 	app.get('/api/friends', function(req, res){
 
-			var data =  orm.allCharacters(function(data){
-				res.json(data); });
-			};
-
-	});
+				res.json(friends); });
+			
 
 	// If a user sends data to add a new character...
 	app.post('/api/friends', function(req, res){
 
 		// Take the request...
 		var character = req.body;
+		//compatibility logic here
 
+		//have a loop
+		//inside, check the total difference between newly added user and the indexed user in the msql database
+		//save the difference in an array
+		//check the array for the lowest value
+		//spit out the index of the person with the lowest difference and return that as the data value.
+		
 		// Then send it to the ORM to "save" into the DB.
 		orm.addCharacter(character, function(data){
 		});
 
-		//compatibility logic here
+
 
 	})
 }
